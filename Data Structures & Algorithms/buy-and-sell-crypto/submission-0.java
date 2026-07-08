019d2a614c9494330b2e132ddb3e9aaff908ea05
Gupendra Verma <69241602+Gupendra-verma@users.noreply.github.com>
2026-07-08 23:47:25 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        //brute force
+
+    int max = 0;
+
+    for(int i=0; i<prices.length; i++){
+
+        for(int j=i+1; j<prices.length; j++){
+
+            max = Math.max(prices[j]-prices[i], max);
+
+        }
+    }
+        
+        return max;
+
+    //     int min = prices[0];
+    //     int max = 0;
+
+    //     for ( int i=1; i<prices.length; i++){
+    //         min = Math.min(min, prices[i]);
+    //         max = Math.max(max, prices[i]-min);
+    //     }
+    // return max;
+        
+    }
+}
